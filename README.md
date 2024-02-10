1827ed600e67513f9d19df99dca05c10c09fa103
ZechsMarquise <carloseduardo.cb91@gmail.com>
2024-02-10 19:41:35 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ms-proxy-form
+
+## Descrição
+
+Esse microserviço é um proxy responsável por enviar os dados para API do Google Forms 
+
+## Pré-requisitos
+
+- Node.js instalado na sua máquina.
+- Gerenciador de pacotes npm ou yarn.
+
+## Instalar Dependencia
+```
+npm i
+```
+
+## Rodar Aplicação
+```
+npm start
+```
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('ms-proxy-forms', async (req, res) => {
+    const formUrl = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdtnAikstVGQXElR_f77KeT3Y_Gjev1LZfe8iWN7DnwJ8Phkg/formResponse';
+
+    try {
+        await axios.post(formUrl, new URLSearchParams(req.body).toString(), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        });
+        // Sucesso na submissão do formulário
+        res.status(200).json({ message: 'Formulário submetido com sucesso!' });
+    } catch (error) {
+        // Erro ao submeter o formulário
+        console.error('Erro ao submeter formulário:', error);
+        res.status(500).json({ message: 'Erro ao submeter formulário' });
+    }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
